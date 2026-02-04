2d9b176dc6a2d9649e0922122e93baf2863e10f4
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2026-02-04 19:56:50 +0100

    test(issue-16): refactor authController.test et ajout JSDoc dans user.mock

--- tests/mocks/user.mock.js
+++ tests/mocks/user.mock.js
@@ -1,32 +1,50 @@
-// Utilitaires de mock pour le modèle User (tests unitaires)
-
 const sinon = require('sinon');
 const User = require('../../src/models/user');
 
-// --- SUCCESS CASES ---
-
+/**
+ * Stub de User.findOne() retournant un résultat simulé.
+ * @param {Object|null} result - Utilisateur simulé ou null
+ */
 function mockFindOne(result) {
     return sinon.stub(User, 'findOne').resolves(result);
 }
 
+/**
+ * Stub de User.create() retournant un résultat simulé.
+ * @param {Object} result - Utilisateur créé simulé
+ */
 function mockCreate(result) {
     return sinon.stub(User, 'create').resolves(result);
 }
 
+/**
+ * Stub de User.findByIdAndDelete() retournant un résultat simulé.
+ * @param {Object|null} result - Utilisateur supprimé simulé ou null
+ */
 function mockDelete(result) {
     return sinon.stub(User, 'findByIdAndDelete').resolves(result);
 }
 
-// --- ERROR CASES ---
-
+/**
+ * Stub de User.findOne() lançant une erreur simulée.
+ * @param {Error} [error] - Erreur simulée
+ */
 function mockFindOneError(error = new Error('DB error')) {
     return sinon.stub(User, 'findOne').throws(error);
 }
 
+/**
+ * Stub de User.create() lançant une erreur simulée.
+ * @param {Error} [error] - Erreur simulée
+ */
 function mockCreateError(error = new Error('DB error')) {
     return sinon.stub(User, 'create').throws(error);
 }
 
+/**
+ * Stub de User.findByIdAndDelete() lançant une erreur simulée.
+ * @param {Error} [error] - Erreur simulée
+ */
 function mockDeleteError(error = new Error('DB error')) {
     return sinon.stub(User, 'findByIdAndDelete').throws(error);
 }
